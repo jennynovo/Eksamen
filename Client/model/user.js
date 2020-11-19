aae2a1a1f9718063ff4586351e4650becd176d78
Jennifer Novosyelok <Jennynovo@MacBook-Pro.local>
2020-11-19 13:41:33 +0100

    test

---
+++ Client/model/user.js
@@ -0,0 +1,9 @@
+class user {
+    constructor(firstName, lastName, age, gender, aboutMe){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.aboutMe;
+    }
+}
