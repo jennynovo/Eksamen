9c3f75f26dad6e931f7fdeefbf114c6579f0b50a
Jennifer Novosyelok <Jennynovo@MacBook-Pro.local>
2020-11-24 12:01:33 +0100

    .

---
+++ client/view/js.js
@@ -0,0 +1,20 @@
+const storageInput =document.querySelector('.storage');
+const text = document.querySelector('.text');
+const button = document.querySelector('.button');
+const storedInput = localStorage.getItem('textinput')
+
+if (storageInput){
+    text.textContent=storageInput
+}
+
+storageInput.addEventListener('input', letter => {
+    text.textContent = letter.target.value
+})
+
+const saveToLocalStorage = ()=> {
+    localStorage.setItem('textinput',text.textContent)
+}
+
+button.addEventListener('click',saveToLocalStorage)
+
+
--- server/server.js
+++ server/server.js
@@ -6,15 +6,15 @@ const PORT = process.env.port || 4000; //her fortæller vi webserveren hvilken p
 const user = require ('../client/model/user.js'); //henter user information - sti
 
 //CRUD ENDPOINTS
-server.get("/user", (req, res) => { //henter information
+server.get("/user.js", (req, res) => { //henter information
     res.json(user)
  });
- server.post("/user", (req, res) => { //opretter user
+ server.post("/user.js", (req, res) => { //opretter user
     res.json(user)
  });
- server.put("user", (req, res) => { //opdaterer user
+ server.put("user.js", (req, res) => { //opdaterer user
     res.json(user)
  });
- server.delete("user", (req, res) => { //sletter user
+ server.delete("user.js", (req, res) => { //sletter user
     res.json(user)
  });
